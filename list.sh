71ba34f778055b9d64d37e2543d154164bbe0a7b
quickfix <devops.july2.2017@gmail.com>
2019-08-12 06:43:17 +0530

    Update list.sh

--- list.sh
+++ list.sh
@@ -3,3 +3,6 @@
 list the artifacts:
 
 curl -X POST -k -H 'Content-Type:text/plain' -H 'X-JFrog-Art-Api: AKCp5dKskeRv2quzhUwiVEs8BwywNDNUbF8QRLwx6Egd6SbJ2c2fV8j3PhAi2x8YD4LX1YStZ' -i 'http://13.235.42.32:8081/artifactory/api/search/aql' --data 'items.find({"repo":"example-repo-local"})'
+
+
+curl -X POST -k -H 'Content-Type:text/plain' -H 'X-JFrog-Art-Api: AKCp5dKskeRv2quzhUwiVEs8BwywNDNUbF8QRLwx6Egd6SbJ2c2fV8j3PhAi2x8YD4LX1YStZ' -i 'http://13.235.42.32:8081/artifactory/api/search/aql' --data 'items.find({"type":"file","created":{"$lt":"2014-05-18T"},"stat.downloaded":{"$lt":"2014-11-18T"},"repo":{"$nmatch":"*release*"},"size":{"$gt":"1000"}})'
